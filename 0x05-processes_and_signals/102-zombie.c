0eadd17f8fe273eea333bc6be09974f01720e606
Sanaalagh <Sanaalaghzou@gmail.com>
2024-01-26 14:07:12 -0800

    The 0x05. Processes and signals  project

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * infinite_while - Creates an infinite loop to keep the program running.
+ *
+ * Return: Always returns 0.
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always returns 0.
+ */
+int main(void)
+{
+	pid_t child_pid;
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		child_pid = fork();
+
+		if (child_pid > 0)
+		{
+			/* Parent process */
+			printf("Zombie process created, PID: %d\n", child_pid);
+			sleep(1);
+		}
+		else
+		{
+			/* Child process (exit immediately to become a zombie) */
+			exit(0);
+		}
+	}
+
+	infinite_while();
+
+	return (0);
+}
---
+++ 0x05-processes_and_signals/README.md
@@ -0,0 +1,1 @@
+It is A README.md file for the  0x05-processes_and_signals  project 
