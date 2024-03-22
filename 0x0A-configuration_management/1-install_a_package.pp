f6d57e39b35c1d9e8e481176c68f8370b46e1bda
Sanaalagh <Sanaalaghzou@gmail.com>
2024-03-22 15:19:26 -0700

    0x0A. Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 # 1-install_a_package.pp
 # This manifest installs Flask version 2.1.0 using pip3
 
-package { 'Flask':
-  ensure   => '3.0.2',
-  provider => 'pip3',
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem',
 }
