6d5d02494e3653c919b23e34b2113d1124066efb
Sanaalagh <Sanaalaghzou@gmail.com>
2024-03-22 16:49:47 -0700

    0x0B-ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,33 @@
-file_line { 'Turn off passwd auth':
-  path  => '/etc/ssh/sshd_config',
-  line  => 'PasswordAuthentication no',
+# Ensure the SSH config directory exists
+file { '/home/vagrant/.ssh':
+  ensure => directory,
+  owner  => 'vagrant',
+  group  => 'vagrant',
+  mode   => '0700',
 }
 
+# Ensure the SSH client configuration file exists
+file { '/home/vagrant/.ssh/config':
+  ensure => file,
+  owner  => 'vagrant',
+  group  => 'vagrant',
+  mode   => '0600',
+}
+
+# Configure SSH to use the private key for authentication
 file_line { 'Declare identity file':
-  path  => '~/.ssh/config',
-  line  => 'IdentityFile ~/.ssh/school',
+  ensure => present,
+  path   => '/home/vagrant/.ssh/config',
+  line   => 'IdentityFile ~/.ssh/school',
+  match  => '^IdentityFile',
+  replace => true,
+}
+
+# Turn off password authentication
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/home/vagrant/.ssh/config',
+  line   => 'PasswordAuthentication no',
+  match  => '^PasswordAuthentication',
+  replace => true,
 }
