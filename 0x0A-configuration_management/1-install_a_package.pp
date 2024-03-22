ebf76b068860d75bc1c47949b6a60fa2ec32eb3a
Sanaalagh <Sanaalaghzou@gmail.com>
2024-03-22 16:56:55 -0700

    some changes

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,8 @@
 # 1-install_a_package.pp
 # This manifest installs Flask version 2.1.0 using pip3
-
 package { 'puppet-lint':
   ensure   => '2.5.0',
+  name     => 'puppet-lint',
   provider => 'gem',
+  source   => 'http://rubygems.org',
 }
