9a02e0ce270a3e0a8a526f9c27068ba50e753569
Sanaalagh <Sanaalaghzou@gmail.com>
2024-03-22 17:00:25 -0700

    some changes

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,7 @@
 #!/usr/bin/pup
 # 1-install_a_package.pp
 # This manifest installs Flask version 2.1.0 using pip3
-package { 'python3-pip':
-  ensure => 'installed',
-}
-
-package { 'flask':
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip',
+  provider => 'pip3'
 }
