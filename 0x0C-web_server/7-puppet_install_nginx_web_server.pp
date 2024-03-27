cdcab582e4f0e670359326470712e710d5701b5d
Sanaalagh <Sanaalaghzou@gmail.com>
2024-03-27 04:32:41 -0700

    updating

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,40 +1,42 @@
+# Puppet script to install and configure Nginx on Ubuntu. It sets up Nginx to listen on port 80, serve a "Hello World!" page at the root, and redirect "/redirect_me" with a 301 to YouTube.
+
 class nginx_setup {
+
   package { 'nginx':
     ensure => installed,
   }
 
-  file { '/var/www/html/index.html':
-    ensure  => file,
-    content => 'Hello World!',
-    require => Package['nginx'],
+  service { 'nginx':
+    ensure     => running,
+    enable     => true,
+    require    => Package['nginx'],
   }
 
-  file { '/etc/nginx/sites-available/default':
+  file { '/var/www/html/index.html':
     ensure  => file,
-    content => "
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    root /var/www/html;
-    index index.html;
-    server_name _;
-    location / {
-        try_files \$uri \$uri/ =404;
-    }
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
-}",
+    content => '<html><body><h1>Hello World!</h1></body></html>',
     require => Package['nginx'],
     notify  => Service['nginx'],
   }
 
-  service { 'nginx':
-    ensure => running,
-    enable => true,
-    require => File['/etc/nginx/sites-available/default'],
+  file { '/etc/nginx/sites-available/default':
+    ensure  => file,
+    content => template('modulename/nginx_default.erb'),
+    require => Package['nginx'],
+    notify  => Service['nginx'],
+  }
+
+  file { '/etc/nginx/snippets/redirect_me.conf':
+    ensure  => file,
+    content => 'return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;',
+    require => Package['nginx'],
+    notify  => Service['nginx'],
+  }
+
+  exec { 'nginx_reload':
+    command     => '/usr/sbin/nginx -s reload',
+    refreshonly => true,
   }
 }
 
 include nginx_setup
-
