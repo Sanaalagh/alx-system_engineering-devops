3ccb1088380e1f2aa6f0698b53e959757e1a7ecb
Sanaalagh <Sanaalaghzou@gmail.com>
2024-03-27 04:38:45 -0700

    updating

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,42 +1,25 @@
-# Puppet script to install and configure Nginx on Ubuntu. It sets up Nginx to listen on port 80, serve a "Hello World!" page at the root, and redirect "/redirect_me" with a 301 to YouTube.
+# Script to install nginx using puppet
 
-class nginx_setup {
-
-  package { 'nginx':
-    ensure => installed,
-  }
-
-  service { 'nginx':
-    ensure     => running,
-    enable     => true,
-    require    => Package['nginx'],
-  }
-
-  file { '/var/www/html/index.html':
-    ensure  => file,
-    content => '<html><body><h1>Hello World!</h1></body></html>',
-    require => Package['nginx'],
-    notify  => Service['nginx'],
-  }
-
-  file { '/etc/nginx/sites-available/default':
-    ensure  => file,
-    content => template('modulename/nginx_default.erb'),
-    require => Package['nginx'],
-    notify  => Service['nginx'],
-  }
-
-  file { '/etc/nginx/snippets/redirect_me.conf':
-    ensure  => file,
-    content => 'return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;',
-    require => Package['nginx'],
-    notify  => Service['nginx'],
-  }
-
-  exec { 'nginx_reload':
-    command     => '/usr/sbin/nginx -s reload',
-    refreshonly => true,
-  }
+package {'nginx':
+  ensure => 'present',
 }
 
-include nginx_setup
+exec {'install':
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
+  provider => shell,
+
+}
+
+exec {'Hello':
+  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
+  provider => shell,
+}
+
+exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/oluwaseundasilva.hashnode.dev\/;\\n\\t}/" /etc/nginx/sites-available/default':
+  provider => shell,
+}
+
+exec {'run':
+  command  => 'sudo service nginx restart',
+  provider => shell,
+}
