4ddd6f837046f5914a41c95ed15001b1a3e54697
Sanaalagh <Sanaalaghzou@gmail.com>
2024-04-12 08:53:36 -0700

    The blog post for Medium platform

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,12 @@
-#!/usr/bin/pup
 # 1-install_a_package.pp
 # This manifest installs Flask version 2.1.0 using pip3
-package {'flask':
+
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',  # Make sure Werkzeug is installed with the compatible version
+  provider => 'pip3',
 }
