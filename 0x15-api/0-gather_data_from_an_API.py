3ab77e00493939818a288f5b8679ab81f772685d
Sanaalagh <Sanaalaghzou@gmail.com>
2024-04-22 04:47:24 -0700

    commiting tasks for the 0x15-Api project

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,48 +3,30 @@
 Gathers data from a REST API for a given employee ID.
 """
 
-import requests
-from sys import argv
-
 if __name__ == "__main__":
-    if len(argv) != 2:
-        print("Usage: {} <employee_id>".format(argv[0]))
-        exit(1)
 
-    employee_id = argv[1]
+    import requests
+    from sys import argv
+    if len(argv) < 2:
+        exit()
+    todos = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}&completed=true"
+        .format(argv[1]))
+    name = requests.get(
+        "https://jsonplaceholder.typicode.com/users?id={}"
+        .format(argv[1]))
+    name = name.json()
+    name = name[0]["name"]
+    todo = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}".format(argv[1]))
+    todo = todo.json()
+    todo = len(todo)
+    todos = todos.json()
+    todo_list = []
 
-    # Endpoint URL for the API
-    url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
-            employee_id)
-
-    # Fetching data from the API
-    response = requests.get(url)
-
-    # Check if the request was successful
-    if response.status_code != 200:
-        print("Error: Unable to fetch data from the API")
-        exit(1)
-
-    try:
-        data = response.json()
-        # Check if data is empty
-        if not data:
-            print("Error: No data found for the provided employee ID")
-            exit(1)
-
-        # Print API response for debugging
-        print("API Response:", data)
-
-        # Count completed tasks
-        completed_tasks = [task for task in data if task.get('completed')]
-        total_tasks = len(data)
-
-        # Displaying employee TODO list progress
-        print(
-                "Employee {} is done with tasks({}/{}):".format(
-                    data[0]['name'], len(completed_tasks), total_tasks))
-        for task in completed_tasks:
-            print("\t", task['title'])
-    except ValueError:
-        print("Error: Failed to parse JSON response from the API")
-        exit(1)
+    for x in todos:
+        todo_list.append("\t {}".format(x["title"]))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, len(todos), todo))
+    for y in todo_list:
+        print(y)
