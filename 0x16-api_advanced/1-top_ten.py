c43e58cdfec832a9c87f93b334817f63947c019d
Sanaalagh <Sanaalaghzou@gmail.com>
2024-05-07 16:01:54 +0000

    the tasks

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,28 +4,39 @@ Module to interact with the Reddit API to get top ten hot posts of a subreddit.
 """
 import requests
 
+
+BASE_URL = 'https://www.reddit.com'
+'''Reddit's base API URL.
+'''
+
+
 def top_ten(subreddit):
-    """ Prints the titles of the top 10 hot posts for a given subreddit. """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'Python:top_ten_posts:v1.0 (by /u/yourusername)'}
-
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-        print("None")
-        return
-
-    data = response.json()
-    posts = data.get("data", {}).get("children", [])
-    if posts:
-        for post in posts:
+    '''Retrieves the title of the top ten posts from a given subreddit.
+    '''
+    api_headers = {
+        'Accept': 'application/json',
+        'User-Agent': ' '.join([
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+            'AppleWebKit/537.36 (KHTML, like Gecko)',
+            'Chrome/97.0.4692.71',
+            'Safari/537.36',
+            'Edg/97.0.1072.62'
+        ])
+    }
+    sort = 'top'
+    limit = 10
+    res = requests.get(
+        '{}/r/{}/.json?sort={}&limit={}'.format(
+            BASE_URL,
+            subreddit,
+            sort,
+            limit
+        ),
+        headers=api_headers,
+        allow_redirects=False
+    )
+    if res.status_code == 200:
+        for post in res.json()['data']['children'][0:10]:
             print(post['data']['title'])
     else:
-        print("None")
-
-if __name__ == "__main__":
-    # This part can be in your 1-main.py, here for demonstration:
-    import sys
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        top_ten(sys.argv[1])
+        print(None)
