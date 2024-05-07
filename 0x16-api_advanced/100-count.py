91639a83ec1e8db9691c928830632422f17bdde5
Sanaalagh <Sanaalaghzou@gmail.com>
2024-05-07 15:36:32 +0000

    the tasks

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 """
-Recursive function to count and sort keywords in the titles of hot articles from a subreddit.
+Recursive function to count and sort keywords
+in the titles of hot articles from a subreddit.
 """
 import requests
 
 
 def count_words(subreddit, word_list, after='', counts={}):
     """
-    Queries the Reddit API, counts occurrences of keywords in hot articles' titles.
+    Queries the Reddit API, counts occurrences of keywords
+    in hot articles' titles.
     """
     headers = {'User-Agent': 'Python:api_advanced:v1 (by /u/yourusername)'}
-    url = (
-        f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100&after={after}"
-    )
+    url = (f"https://www.reddit.com/r/{subreddit}/hot.json?"
+           f"limit=100&after={after}")
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
@@ -26,7 +27,8 @@ def count_words(subreddit, word_list, after='', counts={}):
         for word in word_list:
             word_lower = word.lower()
             # Count occurrences of word in title
-            counts[word_lower] = counts.get(word_lower, 0) + title.count(word_lower)
+            counts[word_lower] = counts.get(
+                word_lower, 0) + title.count(word_lower)
 
     after = data.get('after', None)
     if after is not None:
@@ -51,6 +53,7 @@ if __name__ == "__main__":
     import sys
     if len(sys.argv) < 3:
         print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
-        print("Ex: {} programming 'python java javascript'".format(sys.argv[0]))
+        print(
+            "Ex: {} programming 'python java javascript'".format(sys.argv[0]))
     else:
         result = count_words(sys.argv[1], [x for x in sys.argv[2].split()])
