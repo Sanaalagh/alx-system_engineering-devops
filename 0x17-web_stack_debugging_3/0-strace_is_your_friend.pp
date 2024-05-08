86f9d6a67dbff70adb57e9f1efa56ab4d818785a
Sanaalagh <sanaalaghzou@gmail.com>
2024-05-08 15:42:01 -0700

    some chanes

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,15 +1,6 @@
-# 0-strace_is_your_friend.pp
-# This Puppet manifest fixes missing configuration file issue for Apache on WordPress
-file { '/path/to/missing/config.file':
-  ensure  => file,
-  mode    => '0644',
-  owner   => 'root',
-  group   => 'root',
-  content => "Configuration content goes here\n",
-}
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
 
-exec { 'restart-apache':
-  command     => '/etc/init.d/apache2 restart',
-  refreshonly => true,
-  subscribe   => File['/path/to/missing/config.file'],
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
