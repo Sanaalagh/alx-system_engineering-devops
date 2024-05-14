945234a80e72b1e0d6fd646741d6a2b1b754f612
Sanaalagh <sanaalaghzou@gmail.com>
2024-05-14 02:03:58 -0700

    committing the changes

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,15 @@
+# 0-the_sky_is_the_limit_not.pp
+# This Puppet manifest optimizes Nginx to handle more concurrent connections
+
+exec { 'optimize-nginx-config':
+  command => "/bin/sed -i 's/worker_connections 1024;/worker_connections 2048;/' /etc/nginx/nginx.conf",
+  path    => ['/bin', '/usr/bin'],
+  onlyif  => "/bin/grep -q 'worker_connections 1024;' /etc/nginx/nginx.conf",
+  notify  => Service['nginx'],
+}
+
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Exec['optimize-nginx-config'],
+}
