44b68ac966a7b96d790448155bbbe27a4c2cecf6
Sanaalagh <sanaalaghzou@gmail.com>
2024-05-18 00:49:22 -0700

    remake it

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,14 +1,3 @@
 # This Puppet manifest optimizes Nginx to handle more concurrent connections
-
-exec { 'optimize-nginx-config':
-  command => "/bin/sed -i 's/worker_connections 1024;/worker_connections 2048;/' /etc/nginx/nginx.conf",
-  path    => ['/bin', '/usr/bin'],
-  onlyif  => "/bin/grep -q 'worker_connections 1024;' /etc/nginx/nginx.conf",
-  notify  => Service['nginx'],
-}
-
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => Exec['optimize-nginx-config'],
-}
+exec { '/usr/bin/env sed -i s/15/1000/ /etc/default/nginx': }
+-> exec { '/usr/bin/env service nginx restart': }
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,2 @@
 # This Puppet manifest increases the file descriptor limits for the 'holberton' user
-
-file_line { 'increase-fd-limits':
-  path  => '/etc/security/limits.conf',
-  line  => 'holberton soft nofile 4096\nholberton hard nofile 10000',
-  match => '^holberton.*',
-}
-
-exec { 'apply-sysctl-fs-file-max':
-  command => "/sbin/sysctl -w fs.file-max=100000",
-  unless  => "/sbin/sysctl -n fs.file-max | /bin/grep -q 100000",
-}
+exec { '/usr/bin/env sed -i "s/holberton/foo/" /etc/security/limits.conf': }
